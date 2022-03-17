b1df379b679af28c0b996db10fe3d22c0cface00
Dimas Faidh Muzaki <dimasfaid@gmail.com>
2022-03-17 21:21:56 +0700

    nambahin main

--- DFS_DirCrawler/DFS_DirCrawler/Program.cs
+++ DFS_DirCrawler/DFS_DirCrawler/Program.cs
@@ -14,14 +14,25 @@ namespace DFS_DirCrawler
         [STAThread]
         static void Main()
         {
+            // Pencarian DFS bentuknya instans dari class yang pertama dikonstruk dulu;
             DirCrawlerDFS dirCrawlerDFS = new DirCrawlerDFS();
+            // Pencarian ada 2 method yaitu searchFirst dan searchAll,
+            // Argumennya ada 2 :
+            //    -  string root path belakangnya kasih @
+            //    -  string nama file yang mau dicari pake extensinya
             dirCrawlerDFS.searchFirst(@"D:\Tubes 2 Stima", "test.txt");
+            // Hasilnya nanti objeknya punya dua atribut:
+            //    -  List<string> Paths yang isinya path-path ke file yang ditemuin,
+            //          kalo ga ketemu nanti si Paths nya kosong
+            //    -  Microsoft.Msagl.Drawing.Graph Graph ini isinya graph msagl yang digenerate.
+
+
 
             //create a form 
             System.Windows.Forms.Form form = new System.Windows.Forms.Form();
             //create a viewer object 
             Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
-            //create a graph object 
+            // Disini dipake si Graph dari objek Crawler DFS nya
             Microsoft.Msagl.Drawing.Graph graph = dirCrawlerDFS.Graph;
             //bind the graph to the viewer 
             viewer.Graph = graph;
