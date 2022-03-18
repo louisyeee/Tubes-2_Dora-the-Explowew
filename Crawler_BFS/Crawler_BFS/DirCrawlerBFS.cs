ed9bbc4faca2df5234ab3a5cc868e306a012f170
louisyeee <13520063@std.stei.itb.ac.id>
2022-03-18 08:03:01 +0700

    Add BFS Graph(masih ada bug)

--- Crawler_BFS/Crawler_BFS/DirCrawlerBFS.cs
+++ Crawler_BFS/Crawler_BFS/DirCrawlerBFS.cs
@@ -23,13 +23,13 @@ namespace Crawler_BFS
         public void searchFirst(string rootPath, string fileTarget)
         {
             List<string> results = new List<string>();
-            
+            List<string> nonResults = new List<string>();
             string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.TopDirectoryOnly);
             bool found = false;
             string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.TopDirectoryOnly);
             foreach (string file in files)
             {
-                //graph.AddEdge(rootPath, file);
+                this.drawPohon(rootPath, file);
                 if (Path.GetFileName(file) == fileTarget)
                 {
                     results.Add(file);
@@ -37,30 +37,50 @@ namespace Crawler_BFS
                     Console.WriteLine(file);
                     found = true;
                 }
+                else
+                {
+                    nonResults.Add(file);
+                }
             }
+
             if (!found)
             {
                 foreach (string dir in dirs)
                 {
-                    //graph.AddEdge(rootPath, dir);
+                    this.drawPohon(rootPath, dir);
                     files = Directory.GetFiles(dir, "*.*", SearchOption.TopDirectoryOnly);
                     foreach (string file in files)
                     {
-                        //graph.AddEdge(dir, file);
+                        this.drawPohon(dir, file);
                         if (Path.GetFileName(file) == fileTarget)
                         {
+
                             results.Add(file);
                             Console.WriteLine("Ketemu di: ");
                             Console.WriteLine(file);
                             found = true;
                         }
+                        else
+                        {
+                            nonResults.Add(file);
+                        }
                         if (found)
                         {
                             break;
                         }
+
                     }
 
+                    foreach (string nonRes in nonResults)
+                    {
+                        giveColor(nonRes, rootPath, "red");
+                    }
+                    foreach (string result in results)
+                    {
+                        giveColor(result, rootPath, "blue");
+                    }
                 }
+
                 if (!found)
                 {
                     foreach (string dirNew in dirs)
@@ -68,30 +88,48 @@ namespace Crawler_BFS
                         searchFirst(dirNew, fileTarget);
                     }
                 }
+
             }
+
             this.paths = results;
         }
 
         public void searchAll(string rootPath, string fileTarget)
         {
             List<string> results = new List<string>();
+            List<string> nonResults = new List<string>();
             string[] dirs = Directory.GetDirectories(rootPath, "*", SearchOption.TopDirectoryOnly);
             string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.TopDirectoryOnly);
             foreach (string file in files)
             {
-                //graph.AddEdge(rootPath, file);
+                this.drawPohon(rootPath, file);
                 if (Path.GetFileName(file) == fileTarget)
                 {
                     results.Add(file);
                     Console.WriteLine("Ketemu di: ");
                     Console.WriteLine(file);
                 }
+                else
+                {
+                    nonResults.Add(file);
+                }
+
+
             }
             foreach (string dir in dirs)
             {
-                //graph.AddEdge(rootPath, dir);
+                this.drawPohon(rootPath, dir);
                 searchAll(dir, fileTarget);
             }
+            foreach (string nonRes in nonResults)
+            {
+                giveColor(nonRes, rootPath, "red");
+            }
+            foreach (string result in results)
+            {
+                giveColor(result, rootPath, "blue");
+            }
+
             this.paths = results;
         }
 
@@ -105,6 +143,41 @@ namespace Crawler_BFS
         {
             get { return this.graph; }
             set { this.graph = value; }
+        }
+
+        public void drawPohon(string currPath, string path)
+        {
+            string grand = Path.GetDirectoryName(currPath);
+            this.Graph.AddEdge(Path.GetFileName(currPath) + Path.GetFileName(grand), Path.GetFileName(path) + Path.GetFileName(currPath));
+            this.Graph.FindNode(Path.GetFileName(path) + Path.GetFileName(currPath)).LabelText = Path.GetFileName(path);
+        }
+
+        public void giveColor(string path, string rootPath, string color)
+        {
+            List<string> targetNodes = new List<string>();
+            while (!path.Equals(rootPath))
+            {
+                string parent = Path.GetDirectoryName(path);
+                targetNodes.Add(Path.GetFileName(path) + Path.GetFileName(parent));
+                path = parent;
+            }
+
+            foreach (var edge in this.Graph.Edges)
+            {
+                if (targetNodes.Contains(edge.TargetNode.Id))
+                {
+                    if (color.Equals("red"))
+                    {
+                        edge.Attr.Color = Microsoft.Msagl.Drawing.Color.Red;
+                        edge.TargetNode.Attr.Color = Microsoft.Msagl.Drawing.Color.Red;
+                    }
+                    else if (color.Equals("blue"))
+                    {
+                        edge.Attr.Color = Microsoft.Msagl.Drawing.Color.Blue;
+                        edge.TargetNode.Attr.Color = Microsoft.Msagl.Drawing.Color.Blue;
+                    }
+                }
+            }
         }
     }
 
