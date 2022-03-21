40bec0b44cd2adc028742ff49883183cf58808c1
louisyeee <13520063@std.stei.itb.ac.id>
2022-03-21 17:59:20 +0700

    Fix BFS

--- Crawler_BFS/Crawler_BFS/DirCrawlerBFS.cs
+++ Crawler_BFS/Crawler_BFS/DirCrawlerBFS.cs
@@ -66,7 +66,7 @@ namespace Crawler_BFS
                     found = true;
                     
                     
-                }else if (currFiles.Length == 0 && currDirs.Length == 0)
+                }else
                 {
                     giveColor(currPath, rootPath, "red");
                 }
